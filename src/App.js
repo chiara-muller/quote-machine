b8e463ed0e8ead03c6d2207247e0742f056b8890
chiara-muller <chiaramuller@hotmail.be>
2022-10-19 17:28:02 +0200

    V1 OK

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 import React from 'react';
 import './App.css';
 import quotes from './quotesDB';
-import styles from './styles';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 
 const colors = [
     '#16a085',
@@ -19,53 +22,55 @@ const colors = [
     '#73A857'
   ];
 
+const logo = <FontAwesomeIcon icon={faSquareTwitter} className="TweetButton"/>
+const quoteLogo = <FontAwesomeIcon icon={faQuoteLeft} />
 
 class App extends React.Component {
   constructor(props){  
     super(props);  
+    
     this.state = {  
-         quote: quotes[0].quote,
-         author: quotes[0].author,
-      } 
-    }
+      quote: quotes[0].quote,
+      author: quotes[0].author,
+      color: '#73A857'
+    } 
+  }
 
-    randomQuote = (arr) => {
-      let num = Math.floor(Math.random() * quotes.length)
-      let newQuote = quotes[num]
-      this.setState({
-        quote: newQuote.quote,
-        author: newQuote.author
-      })
-    }
+  randomQuote = (arr) => {
+    let num = Math.floor(Math.random() * quotes.length)
+    let newQuote = quotes[num]
 
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author,
+      color: this.randomColor(colors)
+    })
+  }
 
-    randomColor = (arr) => {
-      let color = Math.floor(Math.random() * colors.length)
-      let newColor = colors[color]
-      return newColor
-    }
-
-    backColor = () => {
-      
-    }
+  randomColor = (arr) => {
+    let color = Math.floor(Math.random() * colors.length)
+    let newColor = colors[color]
+    return newColor
+  }
 
 
   render() {
     return (
-    <div className="App">
-        <header className="App-header" style={{backgroundColor: this.randomColor(colors)}}>
-        <div className="Wrapper">
-          <div className="Quote">'{this.state.quote}'</div>
-          <div className="Author">- {this.state.author}</div>
-          <div className="Footer">
-            <a className="TweetButton" href="#">Tweet</a>
-            <button className="QuoteButton" onClick={this.randomQuote}>New Quote</button>
+      <div className="App">
+        <header className="App-header" style={{backgroundColor: this.state.color, color: this.state.color}}>
+          <div className="Wrapper">
+            <div className="Quote">{quoteLogo} {this.state.quote}</div>
+            <div className="Author">- {this.state.author}</div>
+            <div className="Footer" >
+              <a  href={`https://twitter.com/intent/tweet?text=${this.state.quote}-${this.state.author}`} target="_blank" style={{color: this.state.color}} className="Atag">{logo}</a>
+              <button className="QuoteButton" onClick={this.randomQuote} style={{backgroundColor:this.state.color}}>New quote</button>
+            </div>
           </div>
-        </div>
         </header>
       </div>
     );
-    }
   }
+}
 
 export default App;
+
